5cb4b033d4ef68eee3a7b26e1a594316b86d06fa
Dreiz0x <cisne170990@gmail.com>
2026-04-19 02:51:51 +0000

    🚨 ULTIMATUM: Fix naming conventions and property naming for VespaColors

---
+++ common/src/main/java/dev/vskelk/cdf/core/common/ui/theme/VespaColors.kt
@@ -0,0 +1,13 @@
+package dev.vskelk.cdf.core.common.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object VespaColors {
+    val PRIMARY = Color(0xFFFF6F00)
+    val ACCENT = Color(0xFFFF8F00)
+    val BACKGROUND = Color(0xFF121212)
+    val SURFACE = Color(0xFF1E1E1E)
+    val ON_PRIMARY = Color.White
+    val ON_BACKGROUND = Color.White
+    val ON_SURFACE = Color.White
+}
